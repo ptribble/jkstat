5a336ffa957c3b6131e01f6cf7062737e8610b54
Peter Tribble <peter.tribble@gmail.com>
2023-07-23 11:28:30 +0100

    Current jfreechart doesn't need jcommon

--- jkstat.bat
+++ jkstat.bat
@@ -1,3 +1,3 @@
-set CLASSPATH=.;lib\java\jkstat.jar;lib\java\jcommon-1.0.24.jar;lib\java\jfreechart-1.5.0.jar;lib\java\jingle.jar;lib\java\jumble.jar;lib\java\jstripchart.jar;lib\java\xmlrpc-common-3.1.3.jar;lib\java\xmlrpc-client-3.1.3.jar;lib\java\commons-logging-1.1.1.jar;lib\java\ws-commons-util-1.0.2.jar;lib\java\httpclient-4.2.3.jar;lib\java\httpcore-4.2.2.jar;lib\java\commons-codec-1.6.jar
+set CLASSPATH=.;lib\java\jkstat.jar;lib\java\jfreechart-1.5.0.jar;lib\java\jingle.jar;lib\java\jumble.jar;lib\java\jstripchart.jar;lib\java\xmlrpc-common-3.1.3.jar;lib\java\xmlrpc-client-3.1.3.jar;lib\java\commons-logging-1.1.1.jar;lib\java\ws-commons-util-1.0.2.jar;lib\java\httpclient-4.2.3.jar;lib\java\httpcore-4.2.2.jar;lib\java\commons-codec-1.6.jar
 
 java uk.co.petertribble.jkstat.browser.KstatBrowser -s

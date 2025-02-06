1c59a65185ff977ee73e84f72a698ee642123d42
Peter Tribble <peter.tribble@gmail.com>
2025-02-06 20:39:31 +0000

    Update httpclient to 5.4.2

--- jkstat.bat
+++ jkstat.bat
@@ -1,3 +1,3 @@
-set CLASSPATH=.;lib\java\jkstat.jar;lib\java\jfreechart-1.5.5.jar;lib\java\jingle.jar;lib\java\jumble.jar;lib\java\jstripchart.jar;lib\java\xmlrpc-common-3.1.3.jar;lib\java\xmlrpc-client-3.1.3.jar;lib\java\commons-logging-1.1.1.jar;lib\java\ws-commons-util-1.0.2.jar;lib\java\httpclient5-5.4.1.jar;lib\java\httpcore5-5.3.1.jar;lib\java\httpcore5-h2-5.3.1.jar
+set CLASSPATH=.;lib\java\jkstat.jar;lib\java\jfreechart-1.5.5.jar;lib\java\jingle.jar;lib\java\jumble.jar;lib\java\jstripchart.jar;lib\java\xmlrpc-common-3.1.3.jar;lib\java\xmlrpc-client-3.1.3.jar;lib\java\commons-logging-1.1.1.jar;lib\java\ws-commons-util-1.0.2.jar;lib\java\httpclient5-5.4.2.jar;lib\java\httpcore5-5.3.3.jar;lib\java\httpcore5-h2-5.3.3.jar
 
 java uk.co.petertribble.jkstat.browser.KstatBrowser -s

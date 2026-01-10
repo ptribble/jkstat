1a29acd7e8cf628a8e6fca01faf11b52ee748364
Peter Tribble <peter.tribble@gmail.com>
2026-01-10 20:13:08 +0000

    Clean up wildcard imports for javax.swing.table

--- uk/co/petertribble/jkstat/gui/KstatTableModel.java
+++ uk/co/petertribble/jkstat/gui/KstatTableModel.java
@@ -14,13 +14,13 @@
  *
  * CDDL HEADER END
  *
- * Copyright 2025 Peter Tribble
+ * Copyright 2026 Peter Tribble
  *
  */
 
 package uk.co.petertribble.jkstat.gui;
 
-import javax.swing.table.*;
+import javax.swing.table.AbstractTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
